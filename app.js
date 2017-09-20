f2f1fadba0c940cc9593aaa5c0604d141356dbae
Eason Lai <easlai@microsoft.com>
2017-09-20 18:37:19 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+var express = require('express')
+var app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello! This is NodeJS Sample 01 - NODESAMPLE01')
+})
+
+app.listen(3000, function () {
+  console.log('Example NODESAMPLE01 app listening on port 3000!')
+})
