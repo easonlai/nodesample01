7c6d1c6631416931d47de53698159069d925611c
Eason Lai <easlai@microsoft.com>
2017-11-08 22:11:31 +0800

    update kube9

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello! This is NodeJS Sample 01 - NODESAMPLE01 - kube8')
+  res.send('Hello! This is NodeJS Sample 01 - NODESAMPLE01 - kube9')
 })
 
 app.listen(3000, function () {
